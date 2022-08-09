c09c5b6233ea62c0f0cda4ed468b2dddef1cd4d2
Leonard18aa <67755595+Leonard18aa@users.noreply.github.com>
2022-08-09 15:38:17 -0700

    position fixed

--- src/TwiterSection/TwiterLeftMenu.tsx
+++ src/TwiterSection/TwiterLeftMenu.tsx
@@ -1,46 +1,50 @@
 export function TwitterLeftMenu() {
-    return (
-        <>
-        <div className='twiterLeftPage'>
+  return (
+    <>
+      <div className="twiterLeftPage">
         <button className="TweetLogo">
-        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/4f/Twitter-logo.svg/2491px-Twitter-logo.svg.png" width={30} alt="" />
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/4f/Twitter-logo.svg/2491px-Twitter-logo.svg.png"
+            width={30}
+            alt=""
+          />
         </button>
         <button>
-          <img src="https://cdn-icons-png.flaticon.com/512/15/15766.png" width={20} alt="" />
+          <i className="fa-solid fa-house-user"></i>
           <h3>Home</h3>
         </button>
         <button>
-          <img src="https://w7.pngwing.com/pngs/421/641/png-transparent-sharp-logo-musical-note.png" width={20} alt="" />
+          <i className="fa-regular fa-hashtag"></i>
           <h3>Explore</h3>
         </button>
         <button>
-          <img src="https://toppng.com/uploads/preview/youtube-logo-transparent-square-for-kids-notification-bell-icon-11562857378pfhstovenl.png" width={20} alt="" />
+          <i className="fa-solid fa-bell"></i>
           <h3>Notifications</h3>
         </button>
         <button>
-          <img src="https://toppng.com/uploads/preview/email-icon-transparent-background-11549825133qbltljgp1w.png" width={20} alt="" />
+          <i className="fa-regular fa-envelope"></i>
           <h3>Messages</h3>
         </button>
         <button>
-          <img src="https://cdn3.iconfinder.com/data/icons/web-design-and-development-4-1/512/165-512.png" width={20} alt="" />
+          <i className="fa-thin fa-bookmark"></i>
           <h3>Bookmark</h3>
         </button>
         <button>
-          <img src="https://www.clipartmax.com/png/middle/422-4223422_to-do-list-free-icon-logo-paper-list.png" width={20} alt="" />
+          <i className="fa-thin fa-list-dropdown"></i>
           <h3>Lists</h3>
         </button>
         <button>
-          <img src="https://w7.pngwing.com/pngs/419/473/png-transparent-computer-icons-user-profile-login-user-heroes-sphere-black-thumbnail.png" width={20} alt="" />
+          <i className="fa-thin fa-user"></i>
           <h3>Profile</h3>
         </button>
         <button>
-          <img src="https://cdn-icons-png.flaticon.com/128/152/152528.png" width={20} alt="" />
+          <i className="fa-thin fa-circle-ellipsis"></i>
           <h3>More</h3>
         </button>
         <div className="tweetButton">
-        <button className="tweetButtonTweet">Tweet</button>
+          <button className="tweetButtonTweet">Tweet</button>
         </div>
-        </div>
-        </>
-    )
+      </div>
+    </>
+  );
 }
--- src/TwiterSection/TwiterMainPage.tsx
+++ src/TwiterSection/TwiterMainPage.tsx
@@ -15,7 +15,17 @@ export function TwiterMainPage() {
         </span>
       </div>
       <div className="SecondPost">
-
+      <div className="Username">
+            <img src="https://w7.pngwing.com/pngs/980/304/png-transparent-computer-icons-user-profile-avatar-heroes-silhouette-avatar-thumbnail.png" width={30} alt="" />
+            <p>Nicolass Marcora</p>
+        </div>
+        <img src="https://www.bratislavashootingclub.com/wp-content/uploads/2015/12/steamworkshop_webupload_previewfile_235024238_preview-e1453310947321.jpg" width={550} alt="" />
+        <span className="Comments">
+        <i className="fa-solid fa-comment-dots"></i>
+        <i className="fa-solid fa-retweet"></i>
+        <i className="fa-solid fa-heart"></i>
+        <i className="fa-solid fa-arrow-up-from-bracket"></i>
+        </span>
       </div>
     </div>
   );
